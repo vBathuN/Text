5ee86ff8913eb6979d325395034486cf423fadfa
Mr.TEN <34998996+vBathuN@users.noreply.github.com>
2023-12-30 18:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const {children: titles} = document.querySelector(".animate-text");
+const txtsLen = titles.length;
+let index = 0;
+const textInTimer = 3000;
+const textOutTimer = 2800;
+
+function animateText() {
+  for (let i = 0; i < txtsLen; i++) {
+    titles[i].classList.remove("text-in", "text-out");
+  }
+  titles[index].classList.add("text-in");
+
+  setTimeout(function () {
+    titles[index].classList.add("text-out");
+  }, textOutTimer);
+
+  setTimeout(function () {
+    if (index == txtsLen - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    animateText();
+  }, textInTimer);
+}
+
+window.onload = animateText;
